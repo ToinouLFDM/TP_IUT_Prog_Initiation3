4f63c186f3f097dd82ed3e5e3054a13c012c7f8d
toinoubm <antoine.barbier@epita.fr>
2017-09-29 16:21:41 +0200

    Add files via upload
    rec

--- exo_12/exo_12.c
+++ exo_12/exo_12.c
@@ -24,7 +24,9 @@ Point init_damier(Case map[][H/taille_Case]);
 Point deplacement_player(Case map[][H/taille_Case], Point player, int *isMoving );
 void Game(Case map[][H/taille_Case], Point p, Point m);
 	//fonction Pathfiniding
-
+	Point path_monster(Case map[][H/taille_Case], Point monster, Point player);
+	void path_monster_rec (Case map[][H/taille_Case], int tab[][[H/taille_Case], int i, int j);
+	int min( a,b,c,d);
 //fonction graphique
 void init_ecran(Couleur c);
 void graphisme_case(int i, int j, Couleur c);
@@ -96,6 +98,7 @@ void Game(Case map[][H/taille_Case], Point p, Point m)
 		//printf("is moving %d \n",isMoving );
 		p=deplacement_player(map,p, &isMoving );
 		//printf("is moving %d \n",isMoving );	
+		m=path_monster(map, p, m);
 
 		actualiser();
 		
@@ -156,8 +159,153 @@ Point deplacement_player(Case map[][H/taille_Case], Point p, int *isMoving  )
 	return p;
 }
 		//fonction Pathfiniding
+		Point path_monster(Case map[][H/taille_Case], Point m, Point p)
+		{
+			int tabx1[W/taille_Cas][H/taille_Case]= {0};
+			int tabx2[W/taille_Cas][H/taille_Case]= {0};
+			int taby1[W/taille_Cas][H/taille_Case]= {0};
+			int taby2[W/taille_Cas][H/taille_Case]= {0};
+			if (m.x<([W/taille_Case) && !map[m.x][m.y].wall_r)
+			{	
+				path_monster_rec(map, tabx1, m.x+1, m.y)
+			}
+			if (m.x>0 && !map[m.x-1][m.y].wall_r)
+			{	
+				path_monster_rec(map, tabx2, m.x-1, m.y)
+			}
+			if (m.y<([H/taille_Case) && !map[m.x][m.y].wall_b)
+			{	
+				path_monster_rec(map, taby1, m.x, m.y+1)
+			}
+			if (m.y>0 && !map[m.x][m.y-1].wall_b)
+			{	
+				path_monster_rec(map, taby1, m.x, m.y-1)
+			}
+			int mini = min(tabx1[p.x][p.y],tabx2[p.x][p.y],taby1[p.x][p.y],tabx2[p.x][p.y];
+			switch (mini)
+			{
+				case 1:
+				m.x+=1;
+				break;
+				
+				case 2:
+				m.x -=1;
+				break;
+				
+				case 3:
+				m.y+=1;
+				break;
+				
+				case 3:
+				m.y-=1;
+				break;
+			}
+			graphisme_case(m.x,m.y, jaune);
+			return m;
+		}
+		int min( a,b,c,d)
+		{
+			
+			if (a<b)
+			{
+				if (a<c)
+				{
+					if (a<d)
+					{
+						return 1;
+					}		
+				}
+			}
+			if (b<a)
+			{
+				if (b<c)
+				{
+					if (b<d)
+					{
+						return 2;
+					}		
+				}
+			}
+			if (c<a)
+			{
+				if (c<b)
+				{
+					if (c<d)
+					{
+						return 3;
+					}		
+				}
+			}
+			if (d<a)
+			{
+				if (d<b)
+				{
+					if (d<c)
+					{
+						return 4;
+					}		
+				}
+			}
+		}
 		
+		void path_monster_rec (Case map[][H/taille_Case], int tab[][[H/taille_Case], int i, int j)
+		{
+			if (i-1>=0 && !map[i-1][j].wall_r)
+			{
+		  	if (tab[i-1][j] == 0)
+		  	{
+				  tab[i-1][j]=tab[i][j]+1;
+				  rec_path(tab,i-1,j);
+    		}
+		  	else if (tab[i-1][j] > tab[i][j])
+		  	{
+				  tab[i-1][j]=tab[i][j]+1;
+				  rec_path(tab,i-1,j);
+    		}
+ 		 	}
 
+			if (j-1>=0 && !map[i][j-1].wall_b)
+			{
+				if (tab[i][j-1] == 0)
+				{
+				  tab[i][j-1]=tab[i][j]+1;
+				  rec_path(tab,i,j-1);
+				}
+				else if (tab[i][j-1] > tab[i][j])
+				{
+				  tab[i][j-1]=tab[i][j]+1;
+					rec_path(tab,i,j-1);
+				}
+			}
+
+
+			if (i+1<(W/taille_Case) && !map[i][j].wall_r)
+			{
+				if (tab[i+1][j] == 0)
+				{
+					tab[i+1][j]=tab[i][j]+1;
+					rec_path(tab,i+1,j);
+				}
+				else if (tab[i+1][j] > tab[i][j])
+				{
+					tab[i+1][j]=tab[i][j]+1;
+					rec_path(tab,i+1,j);
+				}
+			}
+
+			if (j+1<(H/taille_Case && !map[i][j].wall_b)
+			{
+				if (tab[i][j+1] == 0)
+				{
+				tab[i][j+1]=tab[i][j]+1;
+				}
+				else if (tab[i][j+1] > tab[i][j])
+				{
+					tab[i][j+1]=tab[i][j]+1;
+					rec_path(tab,i,j+1);
+				}
+			}
+		}
 
 
 //fonction graphique
